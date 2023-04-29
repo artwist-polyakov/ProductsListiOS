ae026cc5b364f48ea2436aa4fcc89b87bb8cb4f3
Aleksandr Poliakov <master@artwist.ru>
2023-04-30 00:52:47 +0300

    Test

--- products list/ViewController.swift
+++ products list/ViewController.swift
@@ -8,6 +8,7 @@
 import UIKit
 
 class ViewController: UIViewController {
+    let words = ["Apple", "Pear", "Watermelon", "Carrot", "Pickle", "Potato", "Tomato"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -15,5 +16,27 @@ class ViewController: UIViewController {
     }
 
 
+}
+
+extension ViewController: UITableViewDataSource {
+    // Здесь будут наши методы dataSource
+    
+    func tableView(_: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return words.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: UITableViewCell
+        if let reusedCell = tableView.dequeueReusableCell(withIdentifier: "cell") {
+            cell = reusedCell
+        } else {
+            cell = UITableViewCell(style: .default, reuseIdentifier: "cell")
+        }
+        cell.textLabel?.text = words[indexPath.row]
+        return cell
+    }
+
+    
+    
 }
 
